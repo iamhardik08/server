6beed88c815ace01cc4902bc024884c7354a7365
Hardik Trivedi <thardik40@gmail.com>
2023-06-08 18:14:59 +0530

    change port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const { register, login } = require('./controllers/authController')
 const app = express()
-const port = 3000;
+const port = 8000;
 
 app.use(express.json());
 
